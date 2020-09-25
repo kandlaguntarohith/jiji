edc6498b1c1e36e32eb2a4550968f75643be3628
urmilshroff <urmilshroff@gmail.com>
2020-09-25 21:01:19 +0530

    Bugfixes

--- lib/pages/my_ads_page.dart
+++ lib/pages/my_ads_page.dart
@@ -111,66 +111,67 @@ class _MyAdsPageState extends State<MyAdsPage> {
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               Padding(
-                padding: EdgeInsets.only(left: 20.0),
-                child: DropdownButton<String>(
-                  hint: Text(_selectedItem ?? 'Select item'),
-                  value: _selectedItem,
-                  onChanged: (String value) {
-                    setState(() {
-                      _selectedItem = value;
-                      print(_selectedItem);
-                    });
-                  },
-                  items: _itemTypes.map((String page) {
-                    return DropdownMenuItem<String>(
-                      value: page,
-                      child: Text(page),
-                    );
-                  }).toList(),
-                ),
-
-                SizedBox(width: 20.0),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    IconButton(
-                      icon: Icon(Icons.edit),
-                      iconSize: 28.0,
-                      onPressed: () => Navigator.of(context).push(
-                        MaterialPageRoute(
-                          builder: (context) => EditProductScreen(
-                            product: new Product(
-                              id: "id1",
-                              description: "This is a very good watch",
-                              imageUrl: [
-                                "https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=989&q=80"
-                              ],
-                              price: 800.00,
-                              title: "Mobile",
-                              state: "Goa",
-                              city: "Pune",
-                              category: "Gadgets",
-                              subCategory: "Mobiles",
+                  padding: EdgeInsets.only(left: 20.0),
+                  child: Column(
+                    children: [
+                      DropdownButton<String>(
+                        hint: Text(_selectedItem ?? 'Select item'),
+                        value: _selectedItem,
+                        onChanged: (String value) {
+                          setState(() {
+                            _selectedItem = value;
+                            print(_selectedItem);
+                          });
+                        },
+                        items: _itemTypes.map((String page) {
+                          return DropdownMenuItem<String>(
+                            value: page,
+                            child: Text(page),
+                          );
+                        }).toList(),
+                      ),
+                      SizedBox(width: 20.0),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          IconButton(
+                            icon: Icon(Icons.edit),
+                            iconSize: 28.0,
+                            onPressed: () => Navigator.of(context).push(
+                              MaterialPageRoute(
+                                builder: (context) => EditProductScreen(
+                                  product: new Product(
+                                    id: "id1",
+                                    description: "This is a very good watch",
+                                    imageUrl: [
+                                      "https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=989&q=80"
+                                    ],
+                                    price: 800.00,
+                                    title: "Mobile",
+                                    state: "Goa",
+                                    city: "Pune",
+                                    category: "Gadgets",
+                                    subCategory: "Mobiles",
+                                  ),
+                                ),
+                              ),
                             ),
                           ),
-                        ),
+                          Text('Edit'),
+                          IconButton(
+                              icon: Icon(Icons.share),
+                              iconSize: 28.0,
+                              onPressed: null),
+                          Text('Share'),
+                          IconButton(
+                              icon: Icon(Icons.send),
+                              iconSize: 28.0,
+                              onPressed: null),
+                          Text('Promote'),
+                        ],
                       ),
-                    ),
-                    Text('Edit'),
-                    IconButton(
-                        icon: Icon(Icons.share),
-                        iconSize: 28.0,
-                        onPressed: null),
-                    Text('Share'),
-                    IconButton(
-                        icon: Icon(Icons.send),
-                        iconSize: 28.0,
-                        onPressed: null),
-                    Text('Promote'),
-                  ],
-
-                ),
-              ),
+                    ],
+                  )),
             ],
           ),
           _selectedItem == 'Listed Items'
