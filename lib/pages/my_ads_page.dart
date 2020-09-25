0660db524bed0f8e805355c7136ca54edfa2154d
kandlaguntarohith <55650704+kandlaguntarohith@users.noreply.github.com>
2020-09-25 11:52:16 +0530

    Added product edit screen

--- lib/pages/my_ads_page.dart
+++ lib/pages/my_ads_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:jiji/components/jijiappbar.dart';
 import 'package:jiji/models/ad_item.dart';
+import 'package:jiji/models/product.dart';
+import 'package:jiji/pages/edit_product_screen.dart';
 import 'package:jiji/widgets/ad_item.dart';
 
 class MyAdsPage extends StatelessWidget {
@@ -60,9 +62,28 @@ class MyAdsPage extends StatelessWidget {
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
                     IconButton(
-                        icon: Icon(Icons.edit),
-                        iconSize: 28.0,
-                        onPressed: null),
+                      icon: Icon(Icons.edit),
+                      iconSize: 28.0,
+                      onPressed: () => Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) => EditProductScreen(
+                            product: new Product(
+                              id: "id1",
+                              description: "This is a very good watch",
+                              imageUrl: [
+                                "https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=989&q=80"
+                              ],
+                              price: 800.00,
+                              title: "Mobile",
+                              state: "Goa",
+                              city: "Pune",
+                              category: "Gadgets",
+                              subCategory: "Mobiles",
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
                     Text('Edit'),
                     IconButton(
                         icon: Icon(Icons.share),
