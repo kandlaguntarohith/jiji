13550b197c10ce222507620a703feec331a84ea4
urmilshroff <urmilshroff@gmail.com>
2020-09-29 21:06:16 +0530

    Add API helper files

---
+++ lib/constants/endpoints.dart
@@ -0,0 +1,11 @@
+class Endpoints {
+  static String baseUrl = "http://happymoney.in";
+
+  static String registerCustomer = "$baseUrl/api/v1/customer/register";
+
+  static String vehicleMakes = "$baseUrl/api/v1/vehicle/makes";
+
+  static String vehicleModels = "$baseUrl/api/v1/vehicle/models";
+
+  static String vehicleAddOns = "$baseUrl/api/v1/vehicle/addons";
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,6 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:hexcolor/hexcolor.dart';
-import 'package:jiji/components/bottomnav.dart';
-import 'package:jiji/pages/chat_page.dart';
-import 'package:jiji/pages/home.dart';
-
-import 'package:flutter/material.dart';
 import 'package:jiji/pages/OnBoardingScreens.dart';
 
 void main() {
