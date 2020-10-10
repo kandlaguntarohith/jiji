02bec7d816885f6e0a91cf467fdc05cd13dd6cd7
PaulAditya <adityapaul1412@gmail.com>
2020-10-10 12:21:29 +0530

    search, like unlike api

--- lib/impl/impl.dart
+++ lib/impl/impl.dart
@@ -40,10 +40,25 @@ class Impl {
     final List<Product> products = await _repository.getPopularProductsList();
     return products;
   }
+    Future<List<Product>> getSearch(
+      Map<String, dynamic> header, Map<String, dynamic> param) async {
+     final List<Product> products = await _repository.getSearch(header, param);
+    return products;
+  }
 
   Future<String> savePost(
       Map<String, dynamic> body, Map<String, String> header) async {
     final String response = await _repository.savePost(body, header);
+    return response;
+  }
+
+  Future<Map<String, dynamic>> putLike(Map<String, String> header, Map<String, dynamic> body) async {
+    final Map<String, dynamic> response = await _repository.putLike(header, body);
+    return response;
+  }
+
+  Future<Map<String, dynamic>> putUnlike(Map<String, String> header, Map<String, dynamic> body) async {
+    final Map<String, dynamic> response = await _repository.putUnlike(header, body);
     return response;
   }
 
