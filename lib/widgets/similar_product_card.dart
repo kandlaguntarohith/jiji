ca7e60d44fec20d222338e621399d1175ab4469b
kandlaguntarohith <55650704+kandlaguntarohith@users.noreply.github.com>
2020-10-13 17:00:27 +0530

    updated

--- lib/widgets/similar_product_card.dart
+++ lib/widgets/similar_product_card.dart
@@ -1,8 +1,12 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
 import 'package:jiji/models/product.dart';
+import 'package:jiji/models/user_model.dart';
 import 'package:jiji/utilities/theme_data.dart';
 import 'package:jiji/pages/product_details.dart';
 import 'package:jiji/utilities/size_config.dart';
+import 'package:provider/provider.dart';
 
 class SimilarProductCard extends StatelessWidget {
   // final String productImgUrl;
@@ -15,8 +19,22 @@ class SimilarProductCard extends StatelessWidget {
     Key key,
     this.product,
   }) : super(key: key);
+
+  bool _isFavourite(UserModel user) {
+    bool isLiked = false;
+    List likedPost = product.likes;
+    likedPost.forEach((element) {
+      if (element.toString().trim() == user.uid.toString().trim()) {
+        isLiked = true;
+      }
+    });
+    return isLiked;
+  }
   @override
   Widget build(BuildContext context) {
+    Box<UserModel> _user = Provider.of<Box<UserModel>>(context, listen: false);
+    UserModel _userModel = _user.values.first;
+    bool isFav = _isFavourite(_userModel);
     String img = product.photo.length > 0
         ? "https://olx-app-jiji.herokuapp.com/api/post/photo/${product.id}?photoId=${product.photo[0].id}"
         : "";
@@ -44,9 +62,25 @@ class SimilarProductCard extends StatelessWidget {
                   width: double.infinity,
                   child: img == ""
                       ? SizedBox()
-                      : Image.network(
-                          img,
+                      : CachedNetworkImage(
+                          filterQuality: FilterQuality.low,
+                          imageUrl: img,
                           fit: BoxFit.cover,
+                          errorWidget: (context, url, error) => SizedBox(
+                            width: double.infinity,
+                            height: double.infinity,
+                            child: Center(
+                              child: Text(
+                                "Image \nNot \nAvailable !",
+                                style: TextStyle(
+                                  fontSize: 5,
+                                  color: MyThemeData.inputPlaceHolder,
+                                ),
+                                textAlign: TextAlign.center,
+                                overflow: TextOverflow.fade,
+                              ),
+                            ),
+                          ),
                         ),
                 ),
               ),
@@ -72,7 +106,7 @@ class SimilarProductCard extends StatelessWidget {
                             overflow: TextOverflow.fade,
                           ),
                           Icon(
-                            true ? Icons.favorite : Icons.favorite_border,
+                            isFav ? Icons.favorite : Icons.favorite_border,
                             color: MyThemeData.primaryColor,
                             size: constraints.maxWidth * 0.06,
                           ),
